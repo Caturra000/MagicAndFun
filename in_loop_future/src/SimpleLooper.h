af00acd23eab7cbca0a5664890bbc4183be580c3
Caturra <wasdjkl233@gmail.com>
2021-08-10 01:44:38 +0800

    [future] add unroll4x for fun

--- in_loop_future/src/SimpleLooper.h
+++ in_loop_future/src/SimpleLooper.h
@@ -1,13 +1,44 @@
 #pragma once
 #include <bits/stdc++.h>
 
+#define CPU_LOOP_UNROLL_4X(actionx1, actionx2, actionx4, width) do { \
+    unsigned long __width = (unsigned long)(width);    \
+    unsigned long __increment = __width >> 2; \
+    for (; __increment > 0; __increment--) { actionx4; }    \
+    if (__width & 2) { actionx2; } \
+    if (__width & 1) { actionx1; } \
+}   while (0)
+
 class SimpleLooper {
 public:
     // example: for(;;) loop();
-    void loop() {
+    void loop(nullptr_t) {
         for(int sz = _mq.size(); sz--;) {
             loopOnce();
         }
+    }
+
+    // just for fun
+    void loop() { unroll4x(); }
+
+    void unroll4x() {
+        size_t n = _mq.size();
+        CPU_LOOP_UNROLL_4X(
+            {
+                loopOnce();
+            },
+            {
+                loopOnce();
+                loopOnce();
+            },
+            {
+                loopOnce();
+                loopOnce();
+                loopOnce();
+                loopOnce();
+            },
+        n);
+
     }
 
     // unsafe
@@ -45,3 +76,5 @@ private:
     std::function<void()>             _lastEvent;
     int                               _global {}; // debug
 };
+
+#undef CPU_LOOP_UNROLL_4X
